3934f55d3cee66ed7d43303561b47c51929c4cc8
Артём <artem@Artems-MacBook-Pro.local>
2019-12-07 18:08:17 +0300

    Get access when we push button

--- src/components/SendButton.js
+++ src/components/SendButton.js
@@ -13,54 +13,40 @@ export class SendButton extends React.Component {
     };
   }
 
-  async componentDidMount() {
-    const stream = await navigator.mediaDevices.getUserMedia({audio: true});
-    this.audio = document.createElement('audio');
-    this.audio.srcObject = stream;
+  async startRecording(event) {
+    event.preventDefault();
 
+    const stream = await navigator.mediaDevices.getUserMedia({audio: true});
     this.mediaRecorder = new MediaRecorder(stream, {
       mimeType: audioType,
     });
-    // init data storage for audio chunks
+
     this.chunks = [];
-    // listen for data from media recorder
     this.mediaRecorder.ondataavailable = event => {
       if (event.data && event.data.size > 0) {
         this.chunks.push(event.data);
       }
     };
-  }
-
-  startRecording(event) {
-    event.preventDefault();
-    // wipe old data chunks
-    this.chunks = [];
-    // start recorder with 10ms buffer
     this.mediaRecorder.start(10);
-    // say that we're recording
     this.setState({recording: true});
   }
 
   stopRecording(event) {
     event.preventDefault();
-    // stop the recorder
+
     this.mediaRecorder.stop();
-    // say that we're not recording
     this.setState({recording: false});
-    // save the audio to memory
     this.saveAudio();
   }
 
   saveAudio() {
     const { audios } = this.state;
-    // convert saved chunks to blob
+
     const blob = new Blob(this.chunks, {type: audioType});
-    // generate audio url from blob
     const audioURL = window.URL.createObjectURL(blob);
  
-    audios.pop();
     audios.push([audioURL]);
-    this.props.handlerAudio(audios);
+    this.props.handlerAudio(audios[audios.length - 1]);
 
     this.setState({audios: audios});
   }
