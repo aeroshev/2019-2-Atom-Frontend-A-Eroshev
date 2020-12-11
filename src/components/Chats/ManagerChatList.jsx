4b3b9ecfc4aa79b6d2006a800a5e0a87eefd1f45
Artem Eroshev <43152363+aeroshev@users.noreply.github.com>
2020-12-11 21:43:38 +0300

    Reference API_URL

--- src/components/Chats/ManagerChatList.jsx
+++ src/components/Chats/ManagerChatList.jsx
@@ -6,7 +6,7 @@ import { ButtonNewChat } from './ButtonNewChat';
 
 export function ManagerChatList(props) {
 	const [chatList, setChatList] = useState([]);
-	const API_URL = 'https://aeroshev.chickenkiller.com';
+	const API_URL = 'http://127.0.0.1:8000';
 
 	async function getChats() {
 		try {
